c8d130435d8a1b1ed7d239049dbd8ff4cb60fbc8
SherlockAkj <michaelakujuo@gmail.com>
2025-07-28 19:51:08 +0100

    Add temporary view for superuser creation on Render

--- multimedia_app/views.py
+++ multimedia_app/views.py
@@ -35,9 +35,9 @@ def multimedia_showcase(request):
 def create_admin_user_temp(request):
     # IMPORTANT: Remove this view immediately after use for security!
     # Choose a unique username and a strong password here
-    admin_username = 'your_admin_username' # <--- CHANGE THIS
+    admin_username = 'admin' # <--- CHANGE THIS
     admin_email = 'admin@example.com'      # <--- CHANGE THIS (optional, but good practice)
-    admin_password = 'your_strong_password' # <--- CHANGE THIS TO A REAL STRONG PASSWORD
+    admin_password = 'michael001' # <--- CHANGE THIS TO A REAL STRONG PASSWORD
 
     if not User.objects.filter(username=admin_username).exists():
         User.objects.create_superuser(admin_username, admin_email, admin_password)
